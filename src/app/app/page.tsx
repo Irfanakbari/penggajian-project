7128b1c2f23840ea76162e11c42d33f042e1d90d
irfanakbari <irfanakbarihabibi18@gmail.com>
2024-10-08 12:16:12 +0700

    rev 1.5 export pdf

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,16 +1,9 @@
 'use client'
-import React, {useState} from "react";
-import {message} from "antd";
+import React from "react";
 
 const App = () => {
-    const [loading, setLoading] = useState(true);
-    const [messageApi, contextHolder] = message.useMessage();
-
     return (
         <>
-            {
-                contextHolder
-            }
             <div
                 className="w-full gap-3 flex flex-col overflow-y-scroll h-full no-scrollbar justify-center items-center backdrop-blur-lg bg-white/20"
                 style={{
@@ -18,7 +11,7 @@ const App = () => {
                     WebkitBackdropFilter: 'blur(30px)', // Mendukung browser yang menggunakan prefix Webkit
                 }}
             >
-                <img src={'/images/logo.png'} alt={'Logo'} width={700} height={700}/>
+                <img src={'images/logo3.jpg'} alt={'Logo'} width={700} height={700}/>
             </div>
         </>
     );
